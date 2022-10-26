0270dc223760902a61b50ddeb16bd2876afa2e6b
Marcela Soriano <97627826+Marcelasoriano@users.noreply.github.com>
2022-10-25 20:22:59 -0400

    Add files via upload

---
+++ circuits1.py
@@ -0,0 +1,6 @@
+#Name: Marcela Soriano Cornejo
+#Email: marcela.sorianocornejo40@myhunter.cuny.edu
+#Date: 17 October 2022
+#Program #23:  Computes the majority of 3 inputs
+
+out = (in3) or ((in1 and in2))
